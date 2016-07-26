83854f32027bee182311ccab1493c992a807b275
zkil <97100170@qq.com>
2016-07-26 17:41:25 +0800

    Initial Commit

---
+++ ZKAlertTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZKAlertTool
+//
+//  Created by lee on 16/7/26.
+//  Copyright © 2016年 sanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZKAlertTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZKAlertTool
+//
+//  Created by lee on 16/7/26.
+//  Copyright © 2016年 sanchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
