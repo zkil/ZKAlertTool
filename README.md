2bc596395df79fe895661605c4b4d622138e6587
zkil <97100170@qq.com>
2017-08-04 17:05:17 +0800

    20170804
    1.优化代码
    2.添加md文件

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # ZKAlertTool
+  
+##简介:  
+基于原生警告框封装，ios8下使用UIAlertView，以上使用UIAlertController  
+##使用方法  
+###导入  
+`#import "ZKAlertTool.h"`
+
+###在任何地方弹出警告框  
+```
+[ZKAlertTool showAlertWithTitle:@"title" andMsg:@"msg" cancelTitle:@"cacel" otherTitles:@[@"1",@"2",@"3",@"4"] handler:^(NSUInteger index) {
+        NSLog(@"tounch %ld",index);
+}];
+```
+
+如果只想显示描述，不处理事件
+```
+[ZKAlertTool showAlertWithMsg:@"msg"];
+```  
+类似其他不需要的值方法查看 `ZKAlertTool.h`
+
+###在指定viewController中弹出警告框
+类似上面，把ZKAlertTool 换成self即可, 详细查看 `"UIViewController+zk_Additions.h`  
+
+```
+[self showAlertWithTitle:@"title" andMsg:@"msg" cancelTitle:@"cacel" otherTitles:@[@"1",@"2",@"3",@"4"] handler:^(NSUInteger index) {
+        NSLog(@"tounch %ld",index);
+}];
+```
+
+###获取顶层控制器
+```
+[UIViewController zk_topViewController];
+```
+或在ViewContoller中  
+```
+[self zk_topViewController];
+```
+
+ 
--- ZKAlertTool/ZKAlertTool.h
+++ ZKAlertTool/ZKAlertTool.h
@@ -6,27 +6,9 @@
 //  Copyright © 2016年 sanchun. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+
 #import <UIKit/UIKit.h>
-
-@interface UIViewController(topViewController)<UIAlertViewDelegate>
-- (void)showAlertWithTitle:(NSString *)title andMsg:(NSString *)msg cancelTitle:(NSString *)cancelTitle otherTitles:(NSArray *)otherTitles handler:(void(^)(NSUInteger index))handler;
-
-- (void)showAlertWithMsg:(NSString *)msg cancelTitle:(NSString *)cancelTitle otherTitles:(NSArray *)otherTitles handler:(void(^)(NSUInteger index))handler;
-- (void)showAlertWithTitle:(NSString *)title andMsg:(NSString *)msg handler:(void(^)(NSUInteger index))handler;
-- (void)showAlert1WithMsg:(NSString *)msg handler:(void(^)(NSUInteger index))handler;
-- (void)showAlert2WithMsg:(NSString *)msg handler:(void(^)(NSUInteger index))handler;
-
-
-
-//普通提示弹窗
-- (void)showAlertWithMsg:(NSString *)msg;
-- (void)showAlertWithTitle:(NSString *)title andMsg:(NSString *)msg;
-
-
-+ (UIViewController *)topViewController;
-- (UIViewController *)topViewController;
-@end
+#import "UIViewController+zk_Additions.h"
 
 @interface ZKAlertTool : NSObject<UIAlertViewDelegate>
 + (instancetype)shareAlertTool;
